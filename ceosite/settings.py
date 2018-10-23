aca1de36b4a5dd7771522bb88acd7ca1a5511b6b
Christina Owens <owens.christina@gmail.com>
2018-10-22 21:06:45 -0700

    Change DEBUG to False, add full url

--- ceosite/settings.py
+++ ceosite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*-&^(sp_j60f(9ismh@$oe&4f!vl4svjukgs0^$p!(rrhbgbzn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'copaspace.pythonanywhere.com']
 
 
 # Application definition
